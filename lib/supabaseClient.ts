223010b8720f5fb5dd0d153f5feacfeec2fae8ca
Cate De Guzman <lr.cdguzman@mmdc.mcl.edu.ph>
2025-07-01 20:29:14 +0800

    Implemented sigleton to Supabase client

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,4 +1,14 @@
-'use client';
-import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
+// lib/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createPagesBrowserClient();
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+let supabase: ReturnType<typeof createClient> | null = null;
+
+export function getSupabaseClient() {
+  if (!supabase) {
+    supabase = createClient(supabaseUrl, supabaseAnonKey);
+  }
+  return supabase;
+}
