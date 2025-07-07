8b1c33a20cc1310f5161de3896e650726ad23c8a
redentordev <redenvalerio2@gmail.com>
2025-07-07 15:59:19 +0800

    Update db folder from drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './src/db/schema.ts',
+  schema: './db/schema.ts',
   out: './supabase/migrations',
   dialect: 'postgresql',
   dbCredentials: {
